36b9b104746a54d40cecf1a4af603ca7f11c4601
Mohammedansari808 <mohammedansari808@gmail.com>
2023-01-05 22:59:39 +0530

    task uploaded

---
+++ index.js
@@ -0,0 +1,53 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+dotenv.config()
+const app = express();
+import { MongoClient } from 'mongodb';
+const PORT = process.env.PORT;
+
+const Mongo_URL = process.env.Mongo_URL;
+//mongodb+srv://mohammedansari808:PRwl2ajPVWhY2jr1@cluster0.q2rstmh.mongodb.net/
+const client = new MongoClient(Mongo_URL)
+await client.connect()
+console.log("Mongo is connected")
+app.get("/", function (request, response) {
+    response.send("🙋‍♂️, 🌏 🎊✨🤩");
+});
+
+app.get("/books", async function (request, response) {
+    const books = await client.db("books").collection("books").find({}).toArray()
+    response.send(books);
+});
+
+app.post("/books/insertbooks", express.json(), async function (request, response) {
+    const data = request.body
+    const books = await client.db("books").collection("books").insertMany(data)
+    response.send(books);
+});
+
+app.put("/books/updatebooks/:id", express.json(), async function (request, response) {
+    const data = request.body
+    const { id } = request.params
+    const books = await client.db("books").collection("books").updateOne({ isbn: id }, { $set: data })
+    response.send(books);
+});
+
+app.delete("/books/deletebook/:id", async function (request, response) {
+    const { id } = request.params
+    const books = await client.db("books").collection("books").deleteOne({ isbn: id })
+    response.send({ message: "successfully deleted" });
+})
+
+app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
+
+
+
+
+
+
+
+
+
+
+
+
